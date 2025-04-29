df11f6fa3587f5347bb8c0b7d472c0a7b5a85ead
shivam4srtech <shivam.srtech@gmail.com>
2025-04-29 11:05:04 +0530

    policy pages

--- src/pages/affiliate-disclosure.js
+++ src/pages/affiliate-disclosure.js
@@ -17,63 +17,40 @@ export default function affiliateDisclosure({ affiliateDisclosure }) {
             <section className="sitePolicy">            
                 <div className="container">                   
                     <div className="row">
-                        <h1 className="text-center">Coupontix Affiliate Disclosure</h1>
-                        <p>At CouponTix, transparency and honesty are the foundation of everything we do. You should know that CouponTix participates in affiliate marketing. This means that when you click on certain links on our website and make a purchase, we may earn a commission or referral fee. 
-                        This Affiliate Disclosure explains how our site works and how we make money. 
-                        </p>
-                        <h2>What is Affiliate Marketing?</h2>
-                        <p>Affiliate marketing is a way for websites like CouponTix to earn by linking products or services offered by other companies (Merchants). When you click our affiliate links and then complete a transaction with the merchant, we may receive a small percentage of the sale or a set fee.</p>
-                        <h2>How do Affiliate Links Work?</h2>
+                        <h1 className="text-center">Affiliate Disclosure</h1>
                         <p>
-                        When you click on a deal, coupon or promotion listed on CouponTix and then make a purchase from the merchant’s website, we may earn an affiliate commission. These links are a part of affiliate programs we participate in. 
+                        At Suproffer, our mission is to help users save money by finding and sharing the best deals, coupons and discounts from around the web. To support our work and continue providing the latest promo codes and offers, we participate in affiliate marketing. 
 
                         </p>
                         <p>
-                        Important points to know:
+                        This Affiliate Disclosure explains how our website works and how we make money.
+                        </p>
+                        <p>
+                        The links you see on our website are affiliate links. If you click one of those links and make a purchase from a third-party website, we may earn a commission - but it does not cost you anything extra. The commission is paid by the retailer or brand. The price of the product or service remains the same whether you use our affiliate link or go directly to the merchant’s website.
+                        </p>
+                        <p>
+                        These commissions help us keep Suproffer running, cover maintenance costs and continue discovering new deals and discounts for our visitors. We are just an affiliate marketing website that brings free coupons and deals for your convenience.
+                        </p>
+                        <p>
+                        It is also important to understand that:
                         </p>
                         <ul>
-                            <li>You do not need to pay any additional costs.
-                            </li>
-                            <li>The price you pay for the product or service will be the same whether you use our link or go directly to the retailer’s website. 
-                            </li>
-                            <li>The commission is paid by the retailer not by you.
-                            </li>
+                            <li>Suproffer does not own, sell or endorse any products or services. </li>
+                            <li>We do not receive free products, perks or incentives from brands in exchange for listing them.</li>
+                            <li>The presence of a coupon or deal does not mean we have a partnership with the brand - we are simply sharing available discounts as part of our affiliate work.</li>
+                            <li>We do not guarantee validity, accuracy, or availability of any coupon or offer listed on our site. Every purchase is between you and the merchant.</li>
                         </ul>
-                        <h2>Why Do We Use Affiliate Links?</h2>
                         <p>
-                        Running and maintaining our website takes resources. From sharing coupons to maintaining website security and performance, there are costs involved. Affiliate marketing helps us to support our operations and continue to provide you with free coupons and deals.
-                        </p>
-                        <h2>No Obligation to Use Our Affiliate Links</h2>
-                        <p>
-                        We appreciate it when you use our affiliate links as it helps keep the platform going. However, there is no obligation to do so. You are free to:
-                        </p>
-                        <ul>
-                            <li>Visit the retailer directly.
-                            </li>
-                            <li>Search for the same offer elsewhere.</li>
-                            <li>Make independent purchasing decisions.</li>
-                        </ul>
-                        <p>We are just an affiliate marketing website that curates coupons and deals for your convenience.</p>
-                        <h2>Our Commitment </h2>
-                        <p>
-                        We are committed to providing you with valuable and helpful information. Our goal is to help you save money. The fact that we may earn a commission does not influence the coupons and offers we feature. We strive to present a wide range of deals and promo codes from various third-party merchants.
+                        Ultimately, the decision to purchase any product or service is yours. You are under no obligation to click on our affiliate links or make a purchase. If you choose to shop through our links, it truly supports what we do and we appreciate it. 
                         </p>
                         <p>
-                        We try to keep everything current but we can’t promise every deal will always be active. Please read the return policy, shipping info, and terms on the retailer’s site before completing a purchase. We are not responsible for product issues, delivery delays or customer service experiences. If anything goes wrong, reach out to the merchant directly.
+                        We always recommend you to read the fine print, check reviews and research before making any purchase. If there is an issue with your order regarding product quality or delivery, you will need to resolve it directly with the seller. 
                         </p>
+                        <div className='h2'>
+                        Got Questions? 
+                        </div>
                         <p>
-                        Please note:
-                        </p>
-                        <ul>
-                                <li>CouponTix does not sell or endorse products. We only link to third-party retailers.</li>
-                                <li>We do not own or produce any of the items or services.</li>
-                                <li>We do not guarantee the quality and reliability of products or services.</li>
-                                <li>We do not receive any free products or perks for including any particular retailer. </li>
-                        </ul>
-                        <h2>Contact Us
-                        </h2>
-                        <p>
-                        If you have any questions about how affiliate links work on CouponTix, please contact us through our <a href="/contact">Contact Page</a> and we will be happy to clarify. 
+                        If you have questions about how affiliate links work, feel free to contact us at <a href="https://suproffer.com/contact">https://suproffer.com/contact. </a>
                         </p>
                     </div> 
                 </div>   
--- src/pages/terms-of-use.js
+++ src/pages/terms-of-use.js
@@ -16,94 +16,107 @@ export default function termsOfUse({ termsOfUse }) {
             <section className="sitePolicy">            
                 <div className="container">                   
                     <div className="row">
-                        <h1 className="text-center">Coupontix Terms of use</h1>
+                        <h1 className="text-center">Terms of Use</h1>
                         <p>
-                            Welcome to CouponTix (the “Website”). These Terms of Use explain how you can use our website and services. By accessing or using CouponTix, you agree to follow the rules outlined here. Please read everything carefully as it outlines your legal rights and obligations. 
+                            Welcome to Suproffer! These Terms of Use lay out the rules and expectations of how you can use our website and the services we offer. By browsing, using or interacting with Suproffer, you are agreeing to follow these terms. Please take a moment to read them carefully. 
                         </p>
-                        <h2>Acceptance of Terms </h2>
+                        <div className='h2'>
+                        Agreement of Terms 
+                        </div>
                         <p>
-                            By using CouponTix in any way - whether browsing, clicking on a deal or using a coupon, you agree to be bound by these Terms of Use. If you do not agree to these terms, please do not use our website. Your continued use of the site means you accept all current terms. 
+                            By using Suproffer (the “Website”), you agree to follow these Terms of Use. If you do not agree with anything stated there, kindly don’t use our site. Using the website means you have read, understood and accepted these terms. 
                         </p>
-                        <h2>Description of Our Service </h2>
-                        <p>CouponTix provides users with coupons, discounts, and promotional offers from various retailers and service providers. We do not sell the products or services offered by the merchants. Our role is to solely provide available coupons and offers to help you save money. </p>
-                        <h2>Use of the Website </h2>
-                        <ul>
-                            <li>You must be 18 years or older to use our site.</li>
-                            <li>You may use CouponTix for your personal, non-commercial use only.</li>
-                            <li>You agree not to use the website for any unlawful purpose or in any way that could harm, disable, or impair the site.</li>
-                            <li>You agree not to attempt to gain unauthorized access to any part of the website, other user accounts, or networks connected to the website.</li>
-                            <li>You agree not to use any automated means such as bots or scrapers to collect information from the website without our written permission.</li>
-                       </ul>
-                       <p>We want to keep the site friendly and fair for everyone. </p>
-                       <h2>Validity of Coupons and Deals   </h2>
-                       <p>We work to keep our deals and promo codes up to date. However, 
-                       </p>
-                       <ul>
-                            <li>
-                                The coupons and deals displayed on CouponTix are provided by the merchants and are subject to their respective terms and conditions including expiry dates and eligibility requirements.
-                            </li>
-                            <li>
-                                CouponTix does not guarantee the validity, accuracy, availability or usability of any coupon or offer.
-                            </li>
-                            <li>
-                                It is your responsibility to verify the terms and conditions of any coupon or offer before making a purchase or using the offer.
-                            </li>
-                            <li>
-                                CouponTix is not responsible for any errors, omissions or misrepresentations made by the merchants regarding their offers.
-                            </li>
-                        </ul>
-                        <h2>Intellectual Property</h2>
+                        <div className='h2'>
+                        What Suproffer Does
+                        </div>
                         <p>
-                        The content on CouponTix including text, graphics, logo, and images is protected by copyright and other intellectual laws. You may not copy, reproduce or distribute any part of our website without written permission. The CouponTix name and logo are trademarks of CouponTix. You may not use these marks without our permission. 
+                        Suproffer is a platform that helps users discover the latest coupons, discounts, and deals from third-party retailers and brands. We do not sell any products or services directly. Our aim is to help you save money by making it easier to find special offers and promo codes all in one convenient place.
+                        </p>
+                        <div className='h2'>
+                        Use of the Website
+                        </div>
+                        <p>
+                        To use Suproffer:
 
                         </p>
-                        <h2>Third-Party Links</h2>
-                        <p>
-                        CouponTix contains links to third-party websites that are not owned or controlled by us. We are not responsible for the content, privacy policies or practices of any third-party websites. You access these websites at your own discretion.
-                        </p>
-                        <h2>Limitation of Liability</h2>
-                        <p>
-                        We do our best to provide reliable information but we cannot guarantee that everything will always be accurate or error-free. 
-                        </p>
                         <ul>
-                            <li>We are not responsible for any losses, damages, or issues that come from using the website or third-party services linked from here. </li>
-                            <li>The site is provided “as is” with no warranties of any kind. 
+                            <li>You need to be at least 18 years old. 
                             </li>
+                            <li>You also agree to use our website only for personal, non-commercial use.</li>
+                            <li>You can’t copy, share or use anything from our website for any commercial purpose without our permission.</li>
+                            <li>Don’t misuse the site or do anything that could harm the site or violate the privacy of other users.</li>
+                            <li>You agree not to use bots, crawlers or scraping tools to collect information from the website without our prior permission.</li>
                         </ul>
-                        <h2>Indemnification</h2>
-                        <p>By using CouponTix, you agree to indemnify and hold harmless CouponTix, its team, partners, and affiliates from and against any claims, losses, damages, obligations, or expenses (including reasonable legal fees) that arise from: 
-                        </p>
-                        <ul>
-                            <li>Your use or access of the website. 
-                            </li>
-                            <li>Your violation of these Terms of Use. </li>
-                            <li>Your infringement of any rights of another person or third party. 
-                            </li>
-                        </ul>
+                        <div className='h2'>
+                        Accuracy of Coupons and Deals
+                        </div>
                         <p>
-                        This means you agree to take full responsibility and cover any costs if your actions cause legal trouble for us. 
+                        We always try our best to make sure coupons and deals listed on Suproffer are accurate and up to date. However, all coupons and promotions come from third-party retailers. We can not guarantee that every coupon will always work or be exactly as described. Offers may change or expire based on the retailer’s own terms.
+                        </p>
+                        <p>
+                            The businesses offering coupons are responsible for them. Any issues you have with a discount code or deal are between you and the retailer. Suproffer is not responsible for any errors or misrepresentations made by the merchants regarding their offers. 
+                            It is your responsibility to verify the terms and conditions of any coupon or offer before making a purchase.
 
                         </p>
-                        <h2>Termination of Use</h2>
-                        <p>We reserve the right to suspend or terminate your access to CouponTix, if
+                        <p>
+                        Additionally, we can change or remove coupons and deals from our website at any time without notice.
+                        </p>
+                        <div className='h2'>
+                        Intellectual Property
+                        </div>
+                        <p>
+                        All the content on Suproffer including name, logo, images, text, and graphics are owned by us and are protected by copyright laws. Please don’t copy, reproduce or share anything from the site. You can’t use our trademarks or logos without our permission. 
+                        </p>
+                        <div className='h2'>
+                        Links to Other Websites
+                        </div>
+                        <p>
+                        You will find third-party links on Suproffer that redirect you to other websites. These are typically retailer sites or offer pages. We don’t control those sites and we are not responsible for their content, privacy policies or practices. Be sure to check their terms and policies and access these websites at your own discretion. 
+                        </p>
+                        <div className='h2'>
+                        Limitation Of Liability
+                        </div>
+                        <p>
+                        Suproffer is provided to you “as is” with no warranties of any kind. While we do our best to keep everything working smoothly, we can’t guarantee that every coupon will be valid or the site will always be error-free. We are not responsible for any problems, losses or damage that come from using the site or clicking on third-party offers.
+                        </p>
+                        <div className='h2'>
+                        Indemnification
+                        </div>
+                        <p>
+                        By using Suproffer, you agree to defend, indemnify and hold us harmless from any claims, damages, losses, liabilities, costs and expenses (including legal fees) that arise from: 
                         </p>
                         <ul>
-                            <li>You violate these Terms. </li>
-                            <li>You engage in harmful or unlawful activity.
+                            <li>Your use of the website. 
                             </li>
-                            <li>We believe your actions put the site or its users at risk. 
-                            </li>
+                            <li>Your violation of these terms. </li>
+                            <li>Any content you submit or share.</li>
+                            <li>Any interactions or transactions you make with third-party sites.</li>
                         </ul>
-                        <h2>Changes to Terms of Use</h2>
                         <p>
-                        We may update these Terms of Use from time to time. We will post any changes on this page and update the “Last Updated” date. Your continued use of the website after any such changes constitutes your acceptance of the new Terms. We recommend you to review these terms periodically. 
+                        Basically, if your actions cause legal trouble for use, you agree to cover it. 
+                        </p>
+                        <div className='h2'>
+                        Termination of Use
+                        </div>
+                        <p>
+                        We reserve the right to suspend or terminate your access to Suproffer with or without notice if we believe you have violated these Terms of Use or misused the site in any way. 
+                        </p>
+                        <div className='h2'>
+                        Changes to Terms of Use
+                        </div>
+                        <p>
+                        We may make changes to these Terms of Use from time to time. If we do, we will update this page. By continuing to use Suproffer after we make changes, you are agreeing to the new terms. We recommend you to review these terms periodically.
+                        </p>
+                        <div className='h2'>
+                        Contact Us
+                        </div>
+                        <p>
+                        If you have any questions about these Terms of Use, please contact us through our <a href="https://suproffer.com/contact">contact page.</a>
+                        </p>
+                        <p>
+                        By using Suproffer, you acknowledge that you have read, understand, and agree to be bound by these Terms of Use. 
 
                         </p>
-                        <h2>Contact Us</h2>
-                        <p>
-                        Have a question or concern? We are here to help! Reach out at <MainDomainLink href="/contact">https://coupontix.com/contact</MainDomainLink>
-                        </p>
-                        <p>By using CouponTix, you acknowledge that you have read and understand these Terms of Use and agree to be bound by them. </p>
                     </div> 
                 </div>   
             </section>
